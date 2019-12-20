b49efeb61870b2112a3dc8c46ee9e27371e0b513
sherif20766 <sherif20766@hotmail.com>
2019-12-20 19:40:08 +0000

    created unit tests for and implemented solution to chunk algorithm

---
+++ src/chunk/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+const chunk = (arr, n) => {
+	const answer = [];
+
+	for (let i = 0; i < arr.length; i + n) {
+		answer.push(arr.splice(i, n));
+	}
+	console.log("answer", answer);
+	return answer;
+};
+
+export default chunk;
