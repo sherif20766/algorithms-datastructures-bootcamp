fc9aa06530ecacd275feb86539a9c5207d5f8648
sherif20766 <sherif20766@hotmail.com>
2019-12-26 18:02:23 +0000

    created unit tests for and implemented solution to matrix algorithm

---
+++ src/matrix/test.js
@@ -0,0 +1,22 @@
+import matrix from "./index";
+
+test("matrix is defined", () => {
+	expect(matrix).toBeDefined();
+});
+
+test("matrix produces a 3x3 array", () => {
+	expect(matrix(3)).toEqual([
+		[1, 2, 3],
+		[8, 9, 4],
+		[7, 6, 5]
+	]);
+});
+
+test("matrix produces a 4x4 array", () => {
+	expect(matrix(4)).toEqual([
+		[1, 2, 3, 4],
+		[12, 13, 14, 5],
+		[11, 16, 15, 6],
+		[10, 9, 8, 7]
+	]);
+});
