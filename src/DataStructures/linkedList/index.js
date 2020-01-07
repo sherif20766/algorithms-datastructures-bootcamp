d6339542aecadf54200d58c64235038033c735b4
sherif20766 <sherif20766@hotmail.com>
2020-01-07 17:53:27 +0000

    implemented Node and unit tests

--- src/DataStructures/linkedList/index.js
+++ src/DataStructures/linkedList/index.js
@@ -2,6 +2,13 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 
 class LinkedList {}
+
+export { Node, LinkedList };
---
+++ src/DataStructures/linkedList/test.js
@@ -0,0 +1,18 @@
+import { Node, LinkedList } from "./index";
+
+test("node is a class", () => {
+	expect(typeof Node.prototype.constructor).toEqual("function");
+});
+
+test("list is a class", () => {
+	expect(typeof LinkedList.prototype.constructor).toEqual("function");
+});
+
+describe("A Node", () => {
+	test("Node has properties data and next", () => {
+		const node = new Node("a", "b");
+
+		expect(node.data).toEqual("a");
+		expect(node.next).toEqual("b");
+	});
+});
