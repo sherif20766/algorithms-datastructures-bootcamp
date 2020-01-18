f9042b8979e00c6ba73947969f26597ad5a2e051
sherif20766 <sherif20766@hotmail.com>
2020-01-18 14:35:33 +0000

    added tests for midpoint function

--- src/DataStructures/midpoint/test.js
+++ src/DataStructures/midpoint/test.js
@@ -1,3 +1,43 @@
 import { Node, LinkedList } from "./linkedList";
 import midpoint from "./index";
 
+test("midpoint is a function", () => {
+	expect(typeof midpoint).toEqual("function");
+});
+
+describe("midpoint returns the middle node of an odd numbered list", () => {
+	const list = new LinkedList();
+
+	test("when the list has 5 elements", () => {
+		list.insertLast(1);
+		list.insertLast(2);
+		list.insertLast(3);
+		list.insertLast(4);
+		list.insertLast(5);
+
+		expect(midpoint(LinkedList).data).toEqual(3);
+	});
+	test("when the list has 7 elements", () => {
+		list.insertLast(6);
+		list.insertLast(7);
+
+		expect(midpoint(LinkedList).data).toEqual(4);
+	});
+});
+
+describe("midpoint returns the middle node of an even numbered list", () => {
+	const list = new LinkedList();
+
+	test("when the list has 2 elements", () => {
+		list.insertLast(1);
+		list.insertLast(2);
+
+		expect(midpoint(LinkedList).data).toEqual(1);
+	});
+	test("when the list has 4 elements", () => {
+		list.insertLast(3);
+		list.insertLast(4);
+
+		expect(midpoint(LinkedList).data).toEqual(2);
+	});
+});
